9297fce9c44d32f74c6ab0284fdcc03195e10952
vishurohilla1 <vanshlrk1@gmail.com>
2026-06-21 19:05:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+mainDiv = document.getElementById("main-div");
+mainDiv.classList = "border w-80 m-3"
+
+input = document.createElement("textarea");
+input.classList = "border m-2 px-2 py-1.5 h-10";
+input.placeholder = "enter your task";
+
+addButton = document.createElement("button");
+addButton.classList = "border h-10 w-20 m-2";
+addButton.innerText = "add task";
+
+tasksArea = document.createElement("div");
+
+addButton.addEventListener("click", addTask);
+
+function addTask() {
+    task = input.value;
+    if (task == "") {
+        alert("enter task to add");
+        return 0;
+    }
+    let taskDiv = document.createElement("div");
+    taskDiv.classList = "flex items-center justify-between m-1 border";
+    let tPara = document.createElement("p");
+    tPara.innerText = task;
+
+    doneBtn = document.createElement("button");
+    doneBtn.innerText = "✅";
+    doneBtn.addEventListener("click",()=>{
+        taskDiv.classList.toggle("bg-gray-400");
+
+    })
+
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "🗑️";
+
+    taskDiv.appendChild(tPara);
+    taskDiv.appendChild(doneBtn);
+    taskDiv.appendChild(delBtn);
+
+    tasksArea.appendChild(taskDiv);
+
+    input.value="";
+
+}
+
+
+
+
+
+
+
+mainDiv.appendChild(input);
+mainDiv.appendChild(addButton);
+mainDiv.appendChild(tasksArea);
